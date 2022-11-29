ff1f70f9a565fe32288e22b9a6c63d0d37e16bc6
Sophie Tyalie <dev@flowerpot.me>
2022-11-29 21:47:52 +0100

    Make session secret be generated at runtime
    The secret for session cookies should not only be a bit longer than the
    previous `secret`, but should also be regenerated at every start.
    Invalidating every session that existed previously. The patch is for
    that
    
    Signed-off-by: Sophie Tyalie <dev@flowerpot.me>

--- server/globals/globals.go
+++ server/globals/globals.go
@@ -1,6 +1,21 @@
 package globals
 
-var Secret = []byte("secret")
+import (
+	"crypto/rand"
+	"log"
+)
+
+func getRandomBytes(length uint) []byte {
+	key := make([]byte, length)
+
+	_, err := rand.Read(key)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return key
+}
+
+var Secret = getRandomBytes(64)
 
 const Userkey = "user"
 
